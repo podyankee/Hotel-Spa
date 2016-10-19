e09c8bde5852bb4e101e2ae6de8deb88940a7cb8
podyankee <podyankee@gmail.com>
2016-10-19 07:07:27 +0300

    first commit Index page finished

---
+++ assets/js/functions.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#gallery a').on('click', function(){
+        var image = $(this).find('img').attr('src');
+        var imageSelected = image.split('thumbnail');
+        var fullPath = (imageSelected[0] + 'fullsize' + imageSelected[1]);
+        $('#galleryModal #imageContainer').attr('src', fullPath);
+    });
+});
+
+$(document).foundation();
