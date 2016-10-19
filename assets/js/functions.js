7f2f49ccfd0b3f67a248b20aa8961d36c0d87418
podyankee <podyankee@gmail.com>
2016-10-19 11:50:12 +0300

    Working with About Us section & overlay

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,10 +1,25 @@
+function getHeight() {
+    var height = $('section.promo, section.promo-video').height();
+    $('#overlay').css({'height': height+'px'})
+}
+
 $(document).ready(function(){
+
+     window.setTimeout('getHeight()',100);
+
+
+
     $('#gallery a').on('click', function(){
         var image = $(this).find('img').attr('src');
         var imageSelected = image.split('thumbnail');
         var fullPath = (imageSelected[0] + 'fullsize' + imageSelected[1]);
         $('#galleryModal #imageContainer').attr('src', fullPath);
     });
+
+    $(window).resize(function  () {
+        getHeight();
+    });
+
 });
 
 $(document).foundation();
